8b483f0bdbaff8c105d3c4ef8a37e9d14e7f5779
Shekhar <kapurshekhar15@gmail.com>
2023-01-14 10:26:03 +0530

    Rate Limiter Implemented

--- UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
@@ -18,6 +18,7 @@ import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.services.UserService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -42,7 +43,8 @@ public class UserController {
 	@GetMapping("/{userId}")
 	// @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod =
 	// "ratingHotelFallback")
-	@Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+	//@Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+	@RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback" )
 	public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
 		logger.info("Get Single User Handler : UserController");
 		logger.info("Retry count: {}", retryCount);
